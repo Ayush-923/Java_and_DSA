31e5a9bc8221b99ced1d78ffeb295a23680487bb
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2023-02-24 22:31:07 +0530

    Add files via upload

---
+++ hashing DS/TreeSetss.java
@@ -0,0 +1,15 @@
+import java.util.TreeSet;
+
+public class TreeSetss {
+    
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Delhi");
+        ts.add("UP");
+        ts.add("MP");
+        ts.add("NOIDA");
+        ts.add("MUMBAI");
+
+        System.out.println(ts);
+    }
+}
