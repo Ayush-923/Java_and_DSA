71a0719ce38c2e1c04a19cb41d1ce841e919ded6
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2023-02-24 22:30:26 +0530

    Add files via upload

---
+++ hashing DS/LinkedHashSett.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+
+public class LinkedHashSett {
+    
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("UP");
+        lhs.add("MP");
+        lhs.add("NOIDA");
+        lhs.add("MUMBAI");
+
+        System.out.println(lhs);
+    }
+}
